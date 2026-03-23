0a17686cd8fb2e1164d2292c274f8c04a19adaf1
chdem <24276208+CharlesDem@users.noreply.github.com>
2026-03-23 15:53:49 +0100

    exo2

---
+++ convert/convert_controller.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, jsonify, request
+from convert.convert_service import celsius_to_fahrenheit, fahrenheit_to_celsius
+
+convert_bp = Blueprint('convert', __name__, url_prefix='/convert/temp')
+
+temps = {'c2f': 'fahrenheit', 'f2c': 'celius'}
+
+@convert_bp.route('', methods=['GET'])
+def convert():
+
+
+    try:
+        value = float(request.args.get('value', ''))
+    except Exception as e:
+        return jsonify({
+            "success": False,
+            "error": "Value must be float"
+        }), 400
+    
+    convert = request.args.get('convert', '').lower()
+
+    if convert  not in temps:
+        return jsonify({
+            "success": False,
+            "error": "c2f not valid"
+        }), 400
+    
+    result = 0.0
+
+    if convert == 'c2f':
+        result = celsius_to_fahrenheit(value)
+    else:
+        result = fahrenheit_to_celsius(value)
+
+    return jsonify({
+            f"{temps.get(convert)}": result,
+        }), 400
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from hello.hello_controller import hello_bp
+from convert.convert_controller import convert_bp
 
 
 app = Flask(__name__)
 
 app.register_blueprint(hello_bp)
+app.register_blueprint(convert_bp)
 
 app.run(debug=True, port=5000)
